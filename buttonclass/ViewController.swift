441b7c40a1d5221e925eb3ab6345a6ec7d71f132
Islam <islam@islams-macbook-pro.local>
2018-11-09 15:33:37 +0300

    Initial Commit

---
+++ buttonclass/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  buttonclass
+//
+//  Created by Islam Dzhabrailov on 11/9/18.
+//  Copyright © 2018 Islam Dzhabrailov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
